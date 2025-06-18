adc050ad71ed3d11217dac999bbacea12a6b7ea8
Ronaldo dias de sousa <ronaldohunter54@gmail.com>
2025-06-18 14:23:48 -0300

    Update postback.js

--- api/postback.js
+++ api/postback.js
@@ -3,9 +3,10 @@
 export default async function handler(req, res) {
   const { evento, id_de_clique, valor } = req.query; // ou req.body se for POST
 
-  console.log('Evento recebido:', evento);
-  console.log('ID do clique:', id_de_clique);
-  console.log('Valor:', valor);
+console.log('Evento recebido:', evento);
+console.log('ID do clique:', id_de_clique);
+console.log('Valentia:', valor);
+
 
   // Aqui você pode salvar no banco, planilha do Google, etc.
 
